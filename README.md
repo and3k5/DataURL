b3a9bfceecdfdff06a4c9d52f193edb7e87ba3aa
Anders K <ande7353@gmail.com>
2015-06-16 07:43:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+     _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _
+      _____            _             _    _   _____    _      
+     |  __ \          | |           | |  | | |  __ \  | |     
+     | |  | |   __ _  | |_    __ _  | |  | | | |__) | | |     
+     | |  | |  / _` | | __|  / _` | | |  | | |  _  /  | |     
+     | |__| | | (_| | | |_  | (_| | | |__| | | | \ \  | |____ 
+     |_____/   \__,_|  \__|  \__,_|  \____/  |_|  \_\ |______|
+     
+     _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _ . _
+
+## What it is
+Drop any file to the page and you will get a data URL of the file.
+
+### Im sorry, what?
+A data URL is often used for images on a page without uploading multiple files.
+
+You can do [stuff like this](data:text/html;base64,PGgxPldlbGNvbWUgdG8gbm93aGVyZTwvaDE+PHA+V2hlcmUgYW0gSSwgeW91IG1pZ2h0IGFzay48YnIgLz5XZWxsIEkgZG9uJ3Qga25vdyA6UDwvcD4=)
+
+Look up [Data URI Scheme](https://en.wikipedia.org/wiki/Data_URI_scheme) at Wikipedia
+## Why?
+I don't know.
+## Need help?
+Any help would be great! 🍴
---
+++ script.js
@@ -0,0 +1,89 @@
+// File upload
+var inputFile = document.querySelector("input[type=file]"); {
+    inputFile.onchange = function (e) {
+        try {
+            e.stopPropagation();
+            e.preventDefault();
+            
+            var files = e.target.files;
+            
+            for (var i=0;i<files.length;i++) {
+                GetFile(files[i]);
+            }
+            
+            
+            return false;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    };
+}
+
+// Drag and drop feature
+var element = document.body; {
+    element.draggable = false;
+    element.ondragover = function (e) {
+        e.preventDefault();
+        return false;
+    };
+    element.ondragleave = element.ondragend = function (e) {
+        return false;
+    };
+    element.ondrop = function (e) {
+        try {
+            e.preventDefault();
+            
+            var files = e.dataTransfer.files;
+            
+            for (var i=0;i<files.length;i++) {
+                GetFile(files[i]);
+            }
+            
+            
+            return false;
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+function fileReaderLoad(e) {
+    var wrap = document.createElement("div");
+    wrap.classList.add("filerow");
+    if (e.target.result.indexOf("image/")===5) {
+        // image
+        var img=wrap.appendChild(document.createElement("div"));
+        img.classList.add("img");
+        img.style.backgroundImage="url('"+e.target.result+"')";
+    }else if (e.target.result.indexOf("audio/")===5) {
+        // audio
+        var aud = wrap.appendChild(new Audio());
+        aud.setAttribute("controls","true");
+        aud.classList.add("col1");
+        aud.src = e.target.result;
+    }else{
+        var fill=wrap.appendChild(document.createElement("div"));
+        fill.classList.add("fill");
+    }
+    
+    var info=wrap.appendChild(document.createElement("div"));
+    var link = info.appendChild(document.createElement("a"));
+        link.textContent="direct link";
+        link.href=e.target.result;
+    
+    info.appendChild(document.createElement("br"));
+    
+    document.querySelector(".preview").appendChild(wrap);
+}
+
+function GetFile(f) {
+    var fileReader = new FileReader();
+    fileReader.onload = fileReaderLoad;
+    fileReader.readAsDataURL(f);
+}
+
+document.querySelector("#clearBtn").addEventListener("click",function () {
+    document.querySelector(".preview").innerHTML="";
+})
